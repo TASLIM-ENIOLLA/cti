a493b83a0d0473f2ff74c64bc950c54789484502
TASLIM-ENIOLLA <61813017+TASLIM-ENIOLLA@users.noreply.github.com>
2022-12-18 17:22:23 +0100

    Update Footer.jsx

--- components/pages/Footer.jsx
+++ components/pages/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer(){
 				<div className = 'row j-c-c'>
 					<div className = 'text-center col-auto text-dark'>
 						<img src = '/vercel.svg' className = 'mb-4' width = '100' />
-						<div>&copy; {new Date().getYear()} All rights reserved. Codehub Technological Institute&reg;</div>
+						<div>&copy; {new Date().getFullYear()} All rights reserved. Codehub Technological Institute&reg;</div>
 					</div>
 				</div>
 			</div>
