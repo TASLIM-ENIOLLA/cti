6b79f3c393d9ed8d29c8d547a0541e3f9699040a
TASLIM-ENIOLLA <taslim.eniolla@gmail.com>
2022-12-19 21:05:05 +0100

    Sixth commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    env: {
+        EMAIL_ADDRESS: 'taslim.eniolla@gmail.com',
+        EMAIL_PASSWORD: 'musaazeezd1st'
+    },
 }
 
 module.exports = nextConfig
